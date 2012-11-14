cb856f2b79b04f38d6368ac7796e024faa612df4
Christian Funkhouser <christian.funkhouser@gmail.com>
2012-11-14 10:25:51 -0500

    Add License (GPL3)

--- stackexchange-schema.sql
+++ stackexchange-schema.sql
@@ -1,3 +1,22 @@
+-- The GNU General Public License 3.0
+--
+-- stackexchange-schema.sql - Nov. 14, 2012 - Schema for separser.py
+--
+-- Copyright (C) 2012 Christian Funkhouser
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
 -- MySQL 5.0+
 -- Create the tables to hold parsed Stack Exchange data
 
